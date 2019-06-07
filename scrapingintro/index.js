b0e79bca06982785b2ba24b351d03ee0a05a7a17
Paul Tobin <tobster111@gmail.com>
2019-06-07 14:21:32 +0100

    Selecting multiple elements

--- scrapingintro/index.js
+++ scrapingintro/index.js
@@ -4,12 +4,13 @@ const cheerio = require('cheerio');
 
 async function main() {
   const html = await request.get(
-    'https://reactnativetutorial.net/css-selectors'
+    'https://reactnativetutorial.net/css-selectors/lesson2.html'
   );
   fs.writeFileSync('./test.html', html);
   const $ = await cheerio.load(html);
-  const theText = $('h1').text();
-  console.log(theText)
+  $('h2').each((index, element) => {
+    console.log($(element).text());
+  });
 }
 
 main();
