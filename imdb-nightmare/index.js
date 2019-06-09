caa9080466377fa5009a81d019aa37ec2d8075b5
Paul Tobin <tobster111@gmail.com>
2019-06-09 18:58:58 +0100

    Getting the url for the high resolution images

--- imdb-nightmare/index.js
+++ imdb-nightmare/index.js
@@ -12,15 +12,47 @@ async function scrapeTitlesRankAndRatings() {
       const title = $(element)
         .find('td.titleColumn > a')
         .text();
-      const imdbRating = Number($(element)
-        .find('td.ratingColumn.imdbRating')
-        .text()
-        .trim());
-      return { title, imdbRating, rank: i };
+
+      const descriptionUrl =
+        'https://www.imdb.com' +
+        $(element)
+          .find('td.titleColumn > a')
+          .attr('href');
+
+      const imdbRating = Number(
+        $(element)
+          .find('td.ratingColumn.imdbRating')
+          .text()
+          .trim()
+      );
+      return { title, imdbRating, rank: i, descriptionUrl };
     })
     .get();
 
+  return movies;
+}
+
+async function scrapePosterUrl(movies) {
+  const moviesWithPosterUrls = await Promise.all(
+    movies.map(async movie => {
+      try {
+        const html = await request.get(movie.descriptionUrl);
+        const $ = await cheerio.load(html);
+        movie.posterUrl =
+          'https://www.imdb.com' + $('div.poster > a').attr('href');
+        return movie;
+      } catch (error) {
+       // console.error(error);
+      }
+    })
+  );
+  return moviesWithPosterUrls;
+}
+
+async function main() {
+  let movies = await scrapeTitlesRankAndRatings();
+  movies = await scrapePosterUrl(movies);
   console.log(movies);
 }
 
-scrapeTitlesRankAndRatings();
+main();
