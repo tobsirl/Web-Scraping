df6edc06ca99c123eeb3b57b86811a54fb6d0b26
Paul Tobin <tobster111@gmail.com>
2019-10-29 19:01:36 +0000

    Simple scraper to get title and rating

--- imdb-request-method/index.js
+++ imdb-request-method/index.js
@@ -1,2 +1,16 @@
 const request = require('request-promise');
 const cheerio = require('cheerio');
+
+const URL = 'https://www.imdb.com/title/tt9335498/?ref_=nv_sr_1?ref_=nv_sr_1';
+
+(async () => {
+  const response = await request(URL);
+
+  let $ = cheerio.load(response);
+
+  let title = $('div[class="title_wrapper"] > h1').text();
+
+  let rating = $('span[itemprop="ratingValue"]').text();
+
+  console.log(title, rating);
+})();
