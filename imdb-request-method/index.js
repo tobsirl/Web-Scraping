8c700ddf0e76957c236d8be10b3d930ab2cccbdf
Paul Tobin <tobster111@gmail.com>
2019-10-30 19:43:12 +0000

    Scraping the url for poster and total ratings

--- imdb-request-method/index.js
+++ imdb-request-method/index.js
@@ -7,13 +7,13 @@ const URL = 'https://www.imdb.com/title/tt9335498/?ref_=nv_sr_1?ref_=nv_sr_1';
   const response = await request({
     uri: URL,
     headers: {
-      "Accept":
+      Accept:
         'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
       'Accept-Encoding': 'gzip, deflate, br',
       'Accept-Language': 'en-IE,en-US;q=0.9,en;q=0.8',
       'Cache-Control': 'no-cache',
-      "Connection": 'keep-alive',
-      "Host": 'www.imdb.com',
+      Connection: 'keep-alive',
+      Host: 'www.imdb.com',
       'Upgrade-Insecure-Requests': '1',
       'User-Agent':
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36'
@@ -21,11 +21,17 @@ const URL = 'https://www.imdb.com/title/tt9335498/?ref_=nv_sr_1?ref_=nv_sr_1';
     gzip: true
   });
 
-  let $ = cheerio.load(response);
+  const $ = cheerio.load(response);
 
-  let title = $('div[class="title_wrapper"] > h1').text();
+  const title = $('div[class="title_wrapper"] > h1')
+    .text()
+    .trim();
 
-  let rating = $('span[itemprop="ratingValue"]').text();
+  const rating = $('span[itemprop="ratingValue"]').text();
 
-  console.log(title, rating);
+  const poster = $('div[class="poster"] > a > img').attr('src');
+
+  const totalRatings = $('div[class="imdbRating"] > a').text();
+
+  console.log({ title, rating, poster, totalRatings });
 })();
