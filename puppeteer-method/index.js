be0b55e5d657aa990aceb53c811cd459382253ce
Paul Tobin <tobster111@gmail.com>
2020-08-24 16:52:56 +0100

    Automating a google search

--- puppeteer-method/index.js
+++ puppeteer-method/index.js
@@ -2,11 +2,17 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: false
+    headless: false,
   });
   const page = await browser.newPage();
-  await page.goto('https://edabit.com');
-  await page.screenshot({path: 'example.png'});
+  await page.goto('https://google.com');
+  await page.type('.gLFyf', 'udemy tutorials', { delay: 100 });
+
+  await page.keyboard.press('Enter');
+
+  await page.waitForNavigation();
+
+  await page.screenshot({ path: 'example.png' });
 
   await browser.close();
 })();
