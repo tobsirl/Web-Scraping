5788fbf49764b2236da6f860343618a4a5560c59
Paul Tobin <tobster111@gmail.com>
2020-08-31 15:04:53 +0100

    Using node-fetch to return the html using await response.text();

--- town-population/index.js
+++ town-population/index.js
@@ -3,4 +3,9 @@ const cheerio = require('cheerio');
 
 const url = `https://www.citypopulation.de/en/ireland/towns/`;
 
-(async () => {})();
+(async () => {
+  const response = await fetch(url);
+  const body = await response.text();
+
+  console.log(body);
+})();
